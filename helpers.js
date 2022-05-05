6ecef02b05d63e73714a5c7ccb1df138b67fac33
OneManEnterprise <102365690+OneManEnterprise@users.noreply.github.com>
2022-05-05 15:05:38 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,18 @@
-console.log("performance.now(): " + performance.now());
+/****************************************************************************************************\
+| ADDRESSES                                                                                           |
+\****************************************************************************************************/
+const bitcoin="3QcD5ZTyWFE6zn8A7KKwn4HLRq5k2dhbuS";
+const bitcoincash ="qzcpvtkka6tnvvvp88qtnmz33kmgf79r9slrtvy82s";
+const dash ="Xbnj3vtVXUnEye8ERBaR2M7dXj8fysnVCo";
+const dogecoin ="D8TKc74V8jFzBFuWpxKyYdaCRRBUxmzjEz";
+const ethereum="0x2fA59Be2Ee252675937e453C5324De1F8849eDb3";
+const litecoin ="MSgS1PYbQD54h1d4as7VZHRpvtNnPdCUDT";
+const solana ="3knrCxQPrjaDrX87uAbGmzLL2GcEGAntddeQWqAwCTuY";
+const zcash ="t1XmjzKxe1ndY6sruWrBu828j4XESfq22FA";
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function now(){console.log("performance.now(): " + performance.now())}
+
+function qSelect(query){return document.querySelector(query)}
+function qSelectAll(query){return document.querySelectorAll(query)}
+
