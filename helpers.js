10f2c8c42c5b7466cce348e51ed4751cbea5f2f2
OneManEnterprise <102365690+OneManEnterprise@users.noreply.github.com>
2022-05-05 18:01:06 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -3,17 +3,21 @@
 /****************************************************************************************************\
 | ADDRESSES                                                                                           |
 \****************************************************************************************************/
-const bitcoin="3QcD5ZTyWFE6zn8A7KKwn4HLRq5k2dhbuS";
-const bitcoincash ="qzcpvtkka6tnvvvp88qtnmz33kmgf79r9slrtvy82s";
-const dash ="Xbnj3vtVXUnEye8ERBaR2M7dXj8fysnVCo";
-const dogecoin ="D8TKc74V8jFzBFuWpxKyYdaCRRBUxmzjEz";
-const ethereum="0x2fA59Be2Ee252675937e453C5324De1F8849eDb3";
-const litecoin ="MSgS1PYbQD54h1d4as7VZHRpvtNnPdCUDT";
-const solana ="3knrCxQPrjaDrX87uAbGmzLL2GcEGAntddeQWqAwCTuY";
-const zcash ="t1XmjzKxe1ndY6sruWrBu828j4XESfq22FA";
+const BITCOIN="3QcD5ZTyWFE6zn8A7KKwn4HLRq5k2dhbuS";
+const BITCOIN_CASH ="qzcpvtkka6tnvvvp88qtnmz33kmgf79r9slrtvy82s";
+const DASH ="Xbnj3vtVXUnEye8ERBaR2M7dXj8fysnVCo";
+const DOGECOIN ="D8TKc74V8jFzBFuWpxKyYdaCRRBUxmzjEz";
+const ETHEREUM ="0x2fA59Be2Ee252675937e453C5324De1F8849eDb3";
+const LITECOIN ="MSgS1PYbQD54h1d4as7VZHRpvtNnPdCUDT";
+const SOLANA ="3knrCxQPrjaDrX87uAbGmzLL2GcEGAntddeQWqAwCTuY";
+const ZCASH ="t1XmjzKxe1ndY6sruWrBu828j4XESfq22FA";
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+//TODO var vs let
+var waitMillis= 2*1000;
+let waitIterations = 10;
 
 function now(){console.log("performance.now(): " + performance.now())}
+function wait(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
 function qSelect(query){return document.querySelector(query)}
 function qSelectAll(query){return document.querySelectorAll(query)}
@@ -26,4 +30,14 @@ function dSelectAll(query){
   console.debug("query: " + query); 
   document.querySelectorAll(query).forEach(el => console.debug(el));
   return document.querySelectorAll(query);
+}
+async function qqSelect(query){
+    let element = qSelect(query);
+    let iteration = 0;
+    while(!element){
+        if(iteration == waitIterations) return;
+        
+        element = qSelect(query);
+        await wait(waitMillis);
+        iteration++;
 }
