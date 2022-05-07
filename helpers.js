db0ec57bf3100e2920531b2b38723526d7d02890
OneManEnterprise <102365690+OneManEnterprise@users.noreply.github.com>
2022-05-07 14:32:59 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,37 +1,35 @@
-//@require https://raw.githubusercontent.com/OneManEnterprise/MonkeyBrain/main/helpers.js
+//@require       https://raw.githubusercontent.com/OneManEnterprise/MonkeyBrain/main/helpers.js
 
 /****************************************************************************************************\
 | CONSTANTS                                                                                           |
 \****************************************************************************************************/
 //ADDRESSES
-const BITCOIN="3QcD5ZTyWFE6zn8A7KKwn4HLRq5k2dhbuS";
-const BITCOIN_CASH ="qzcpvtkka6tnvvvp88qtnmz33kmgf79r9slrtvy82s";
-const DASH ="Xbnj3vtVXUnEye8ERBaR2M7dXj8fysnVCo";
-const DOGECOIN ="D8TKc74V8jFzBFuWpxKyYdaCRRBUxmzjEz";
-const ETHEREUM ="0x2fA59Be2Ee252675937e453C5324De1F8849eDb3";
-const LITECOIN ="MSgS1PYbQD54h1d4as7VZHRpvtNnPdCUDT";
-const SOLANA ="3knrCxQPrjaDrX87uAbGmzLL2GcEGAntddeQWqAwCTuY";
-const ZCASH ="t1XmjzKxe1ndY6sruWrBu828j4XESfq22FA";
+const ADDR_BITCOIN="3QcD5ZTyWFE6zn8A7KKwn4HLRq5k2dhbuS";
+const ADDR_BITCOIN_CASH ="qzcpvtkka6tnvvvp88qtnmz33kmgf79r9slrtvy82s";
+const ADDR_DASH ="Xbnj3vtVXUnEye8ERBaR2M7dXj8fysnVCo";
+const ADDR_DOGECOIN ="D8TKc74V8jFzBFuWpxKyYdaCRRBUxmzjEz";
+const ADDR_ETHEREUM ="0x2fA59Be2Ee252675937e453C5324De1F8849eDb3";
+const ADDR_LITECOIN ="MSgS1PYbQD54h1d4as7VZHRpvtNnPdCUDT";
+const ADDR_SOLANA ="3knrCxQPrjaDrX87uAbGmzLL2GcEGAntddeQWqAwCTuY";
+const ADDR_ZCASH ="t1XmjzKxe1ndY6sruWrBu828j4XESfq22FA";
 
 //QUERIES
 const Q_HCAPTCHA = "iframe[data-hcaptcha-widget-id]";
-const BTN = "button";
-const IN = "input";
-const ADDR = "[name='address']";
-const SUB = "[type='submit']";
-const ID_SUB = "#submit";
+const Q_BTN = "button";
+const Q_IN = "input";
+const Q_ADDR = "[name='address']";
+const Q_SUB = "[type='submit']";
 
 const Q_BTN_SUB = BTN + SUB;
 const Q_BTN_ID_SUB = BTN + ID_SUB;
 const Q_IN_SUB = IN + SUB;
+
+//IDS
+const ID_SUB = "#submit";
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//TODO var vs let
-var waitMillis= 2*1000;
-let waitIterations = 10;
-
-function now(){console.log("performance.now(): " + performance.now())}
+let waitMillis= 2*1000;
 function wait(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
+
 function isAtUrl(url){ return window.location.href == url}
 function containsUrl(url){ return window.location.href.includes(url)}
 
