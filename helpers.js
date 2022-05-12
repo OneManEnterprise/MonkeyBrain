2c02288a24fb9461f5179398dc938349312c4dd1
OneManEnterprise <102365690+OneManEnterprise@users.noreply.github.com>
2022-05-12 13:24:11 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -142,6 +142,18 @@ function scrollIntoMidView(element) {
   window.scrollTo(0, scrollY);
 }
 
+function convertMS(ms) {
+  var d, h, m, s;
+  s = Math.floor(ms / 1000);
+  m = Math.floor(s / 60);
+  s = s % 60;
+  h = Math.floor(m / 60);
+  m = m % 60;
+  d = Math.floor(h / 24);
+  h = h % 24;
+  return { d: d, h: h, m: m, s: s };
+};
+
 String.prototype.nthLastIndexOf = function(searchString, n){
   if(this === null) return -1;
   //if(typeof(this) != "string") return -1;
